20c1c9e0c4d66c12a1f5c73c3c01280b16532769
Michel Megens <dev@michelmegens.net>
2012-02-18 13:46:39 +0100

    error.h: header definitions. debug.h: lib debug functions.

---
+++ debug.h
@@ -0,0 +1,13 @@
+/**
+  * \author Michel Megens
+  * \copyright 2012
+  */
+  
+#ifndef __DEBUG_H
+#define __DEBUG_H
+static int init_list();
+
+static void print_node(int it, NODE *node);
+
+static void destroy_list();
+#endif
---
+++ error.h
@@ -0,0 +1,16 @@
+/**
+ * \file error.h
+ * \author Michel Megens
+ */
+
+#ifndef __ERROR_H
+#define __ERROR_H
+
+typedef enum
+{
+        OK = 0,
+        GENERAL_ERR,
+        NO_MEM,
+} error_t;
+
+#endif
